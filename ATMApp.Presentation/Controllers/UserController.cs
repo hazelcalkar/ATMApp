357196ec6cdb507185c3dd92558630471b6fad0b
hazel çalkar <hazelclkr@gmail.com>
2023-01-17 14:16:05 +0300

    Add project files.

---
+++ ATMApp.Presentation/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using ATMApp.Application.Models.DTOs;
+using ATMApp.Application.Services.UserService;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ATMApp.Presentation.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+
+        public UserController(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> CreateUser(CreateUserDTO model)
+        {
+            if (ModelState.IsValid)
+            {
+                await _userService.CreateUser(model);
+
+                return Ok("kullanıcı başarılı bir şekilde eklenmiştir.");
+            }
+            return StatusCode(500);
+        }
+    }
+}
